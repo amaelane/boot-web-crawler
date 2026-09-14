8a32913308df29834552c1b8c511a8a275a99479
Amanda <samcarter5224@comcast.net>
2026-09-14 13:08:28 -0500

    Implement argument validation in main function
    Add command-line argument handling for website input.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
     print("Hello from boot-web-scraper!")
 
+    import sys
+
+    print("Script name:", sys.argv[0]) # example.py
+    print("Arguments:", sys.argv[1:]) # -v
+
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    if len(sys.argv) == 2:
+        print(f"starting crawl of: {sys.argv[1]}")
+
 
 if __name__ == "__main__":
     main()
