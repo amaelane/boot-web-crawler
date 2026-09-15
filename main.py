a2049c89939a1586fc2eb595a7b39434ec2e0465
Amanda <samcarter5224@comcast.net>
2026-09-15 16:16:29 -0500

    Progress on main.py refactor for lesson 8

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import sys
+import requests
+
+headers = {"User-Agent": "BootCrawler/1.0"}
+
 def main():
-    print("Hello from boot-web-scraper!")
+    print("Welcome to boot-web-scraper!")
 
-    import sys
-
-    print("Script name:", sys.argv[0]) # example.py
-    print("Arguments:", sys.argv[1:]) # -v
+    #print("Script name:", sys.argv[0]) # example.py
+    #print("Arguments:", sys.argv[1:]) # -v
 
     if len(sys.argv) < 2:
         print("no website provided")
@@ -15,8 +18,19 @@ def main():
         sys.exit(1)
 
     if len(sys.argv) == 2:
-        print(f"starting crawl of: {sys.argv[1]}")
+        print(f"Starting crawl of: {sys.argv[1]}")
 
+    get_html(sys.argv[1])
+
+
+def get_html(url) -> str:
+    response = requests.get(url, headers=headers)
+
+    if response.status_code >= 400:
+        raise Exception(f"Error fetching {url}: {response.status_code}")
+    elif response.headers.get("Content-Type") != "text/html":
+        raise Exception(f"Error fetching {url}: Content-Type is not text/html")
+    return response.text
 
 if __name__ == "__main__":
     main()
